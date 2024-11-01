d98586504882e28cd587779573fccf3317726b65
Frank Mayer <gh@frankmayer.dev>
2024-11-01 22:21:50 +0100

    defer :)

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,15 @@
+#include "defer.h"
 #include <iostream>
 
+struct Meep {
+  std::string foo = "Meep!";
+};
+
 int main() {
-  std::cout << "Hello, World!" << std::endl;
+  Meep *meep = new Meep();
+  DEFER(delete meep);
+
+  DEFER(std::cout << "Goodbye, " << meep->foo << std::endl);
+  std::cout << "Hello, " << meep->foo << std::endl;
   return 0;
 }
